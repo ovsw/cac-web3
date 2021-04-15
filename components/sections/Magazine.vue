35a2e223f54109d8b0595db5dd3c6707b8471d3c
Ovi Savescu <ovi@ovswebsites.com>
2021-04-15 06:29:18 +0300

    improved magsection styles

--- components/sections/Magazine.vue
+++ components/sections/Magazine.vue
@@ -8,7 +8,8 @@
     data-theme="frame"
     data-theme-color="green"
   >
-    <div class="[ magSection__image ] relative flex-grow">
+    <div class="[ magSection__image ] relative flex-1 2xl:ml-grd">
+      <!-- FIXME: images look like shit -->
       <img
         class="object-cover w-full h-full"
         :src="
@@ -43,11 +44,11 @@
               .height(1500) +
             ' 2500w'
         "
-        sizes="
+      />
+        <!-- sizes="
           (min-width: 1500px) 860px,
           100vw
-        "
-      />
+        " -->
 
       <client-only v-if="section.video">
         <silent-box
@@ -76,14 +77,14 @@
 
     <div
       class="[ magSection__content ] 
-        flex-auto 
-        border-2 border-green
+        flex-1
+        border-4 border-green
 
-        my-grd p-6 space-y-6
+        m-grd p-6 space-y-6
 
         md:border-5 md:p-10 
         
-        lg:max-w-3xl lg:p-20  lg:ml-grd lg:my-0
+        lg:max-w-2xl lg:p-20   lg:my-0
         "
     >
       <div class="flow">
