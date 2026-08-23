d824bcb3cf23c0262dccf40f8785d1347f141ecc
OVIS <ovi@ovswebsites.com>
2026-08-23 18:10:28 +0300

    Fix GA4 page views

--- plugins/vue-gtag.js
+++ plugins/vue-gtag.js
@@ -3,11 +3,46 @@ import VueGtag from 'vue-gtag'
 
 export default ({ app }) => {
   Vue.use(VueGtag, {
-    config: { id: 'G-XEJ31NQ67M' },
-    pageTrackerTemplate: route => ({
-      page_location: window.location.href,
-      page_path: route.fullPath,
-      page_title: document.title
+    config: { id: 'G-XEJ31NQ67M' }
+  })
+
+  let lastTrackedPath
+  const pendingPaths = new Set()
+
+  const trackPageView = (route, from) => {
+    if (lastTrackedPath === route.fullPath || pendingPaths.has(route.fullPath)) {
+      return
+    }
+
+    pendingPaths.add(route.fullPath)
+    const previousTitle = from && document.title
+
+    const sendPageView = () => {
+      lastTrackedPath = route.fullPath
+      pendingPaths.delete(route.fullPath)
+      Vue.$gtag.pageview({
+        page_location: new URL(route.fullPath, window.location.origin).href,
+        page_path: route.fullPath,
+        page_title: document.title
+      })
+    }
+
+    const sendWhenTitleIsReady = (frame = 0) => {
+      if (!previousTitle || document.title !== previousTitle || frame >= 120) {
+        sendPageView()
+        return
+      }
+
+      requestAnimationFrame(() => sendWhenTitleIsReady(frame + 1))
+    }
+
+    Vue.nextTick(() => {
+      requestAnimationFrame(() => sendWhenTitleIsReady())
     })
-  }, app.router)
+  }
+
+  window.onNuxtReady(nuxt => {
+    trackPageView(app.router.currentRoute)
+    nuxt.$on('routeChanged', trackPageView)
+  })
 }
